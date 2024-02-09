9e75647e93b19ed694c482f9e42f6d5e6ed3b117
Marc <manjaroman2@protonmail.com>
2024-02-09 20:34:38 +0100

    all

--- load_patch.sh
+++ load_patch.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 source patch.txt
-if [[ -z "$exdir" ]] || [[ -z "$gitpatch" ]] || [[ ! -z "$pck_file" ]]; then 
+if [[ -z "$exdir" ]] || [[ -z "$gitpatch" ]] || [[ -z "$pck_file" ]]; then 
     echo "supply exdir, gitpatch and pck_file in patch.txt" 
     exit 1
 fi
+
+exdir=$(realpath $exdir)
+./decomp-3.5.3.sh $pck_file $exdir
+
 name=$(basename ${gitpatch})
 name="${name%.*}"
 echo exdir=$exdir 
 echo gitpatch=$gitpatch
 echo name=$name
-git clone $gitpatch && chmod +x $name/setup.sh 
+git clone $gitpatch
 
-./decomp-3.5.3.sh $pck_file $exdir
+cat <<EOF >$name/common.py
+exdir=\"$(realpath $exdir)\"
+EOF
 
-chmod +x $name/setup.sh 
+chmod +x $name/setup.sh $exdir $(realpath $name)
 $name/setup.sh 
 
 echo "python $2/apply.py" 
